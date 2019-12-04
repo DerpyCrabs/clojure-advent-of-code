7037fa65073f9b81c5cef0a96c45115328ed53cd
DerpyCrabs <derpycrabs@gmail.com>
2019-12-04 21:29:59 +0700

    Complete day 4 solution

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,6 +2,7 @@
   (:require [clojure-advent-of-code.day1 :as day1])
   (:require [clojure-advent-of-code.day2 :as day2])
   (:require [clojure-advent-of-code.day3 :as day3])
+  (:require [clojure-advent-of-code.day4 :as day4])
   )
 
 
---
+++ src/clojure_advent_of_code/day4.clj
@@ -0,0 +1,37 @@
+(ns clojure-advent-of-code.day4)
+
+(def input [171309 643603])
+(defn has-same-adjacent-digits [pass]
+  (loop [n 0]
+    (if (= n 5)
+      false
+      (if (= (nth pass n) (nth pass (inc n)))
+        true
+        (recur (inc n))))))
+
+(defn digits-doesnt-decrease [pass]
+  (apply <= (map #((comp read-string str) %1) pass)))
+
+(defn solution []
+  (loop [i (first input) count 0]
+    (if (= i (second input))
+      count
+      (if (and (has-same-adjacent-digits (str i)) (digits-doesnt-decrease (str i)))
+        (recur (inc i) (inc count))
+        (recur (inc i) count)))))
+
+(defn has-only-two-adjacent-digits [pass]
+  (loop [n 0 last-digit nil]
+    (if (= n 5)
+      false
+      (if (and (= (nth pass n) (nth pass (inc n))) (not= (nth pass (+ n 2) nil) (nth pass n)) (not= last-digit (nth pass n)))
+        true
+        (recur (inc n) (nth pass n))))))
+
+(defn solution2 []
+  (loop [i (first input) count 0]
+    (if (= i (second input))
+      count
+      (if (and (has-only-two-adjacent-digits (str i)) (digits-doesnt-decrease (str i)))
+        (recur (inc i) (inc count))
+        (recur (inc i) count)))))
